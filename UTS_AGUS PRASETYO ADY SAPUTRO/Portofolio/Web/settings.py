07259d17d7397fb66f7ba8d0b91d71090c4aa033
Agus Prasetyo Ady Saputro <agusprasetyoadysaputro87@gmail.com>
2026-06-12 22:18:24 +0700

    bissmilah ngak revisi lagi

--- UTS_AGUS PRASETYO ADY SAPUTRO/Portofolio/Web/settings.py
+++ UTS_AGUS PRASETYO ADY SAPUTRO/Portofolio/Web/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$j*(c#1192n+5jx%7nyjl@8!s2ruse*3v3-vzmrf$6bxakd1le'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
